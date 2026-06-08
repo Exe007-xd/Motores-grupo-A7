bce37d7c29aaa333948254ed2f9a0e029573c83c
Exe007-xd <exequielbaez007@gmail.com>
2026-06-08 13:06:15 -0300

    Ajustes de escena + IA basica

---
+++ Assets/Scripts/EnemyScripts/EnemyPatrol.cs
@@ -0,0 +1,105 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent navMeshAgent;
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject[] destinations;
+    [SerializeField] private float distanceToFollowPlayer = 5f;
+    [SerializeField] private float waitTime = 2f;
+
+    private int currentDestination = 0;
+
+    private float waitCounter;
+    private bool waiting;
+
+    void Start()
+    {
+        navMeshAgent.SetDestination(destinations[currentDestination].transform.position);
+    }
+
+  
+    void Update()
+    {
+        // =========================
+        // ESPERA
+        // =========================
+
+        if (waiting)
+        {
+            waitCounter -= Time.deltaTime;
+
+            if (waitCounter <= 0)
+            {
+                waiting = false;
+
+                currentDestination++;
+
+                if (currentDestination >= destinations.Length)
+                {
+                    currentDestination = 0;
+                }
+
+                navMeshAgent.SetDestination(
+                    destinations[currentDestination].transform.position
+                );
+            }
+
+            return;
+        }
+
+        // =========================
+        // DETECCIÓN JUGADOR
+        // =========================
+
+        if (Vector3.Distance(player.transform.position, transform.position) < distanceToFollowPlayer)
+        {
+            navMeshAgent.SetDestination(player.transform.position);
+            return;
+        }
+
+        // =========================
+        // DETECTA LLEGADA
+        // =========================
+
+        if (!navMeshAgent.pathPending &&
+            navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance &&
+            (!navMeshAgent.hasPath || navMeshAgent.velocity.sqrMagnitude == 0f))
+        {
+            waiting = true;
+            waitCounter = waitTime;
+        }
+
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (player == null)
+            return;
+
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+
+        // Cambia de color si detecta al jugador
+        if (distance < distanceToFollowPlayer)
+        {
+            Gizmos.color = Color.red;
+        }
+        else
+        {
+            Gizmos.color = Color.green;
+        }
+
+        // Radio de detección
+        Gizmos.DrawWireSphere(transform.position, distanceToFollowPlayer);
+
+        // Línea hacia el jugador
+        Gizmos.DrawLine(transform.position, player.transform.position);
+    }
+
+    
+   
+    
+    
+}
